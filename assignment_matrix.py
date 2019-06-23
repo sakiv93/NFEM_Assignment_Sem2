c03b8e5849c44f592dcc27bda9e32e2cb594475c
sakiv93 <vikas.sakiv93@gmail.com>
2019-06-23 12:10:16 +0200

    Algoritm for assignment matrix

---
+++ assignment_matrix.py
@@ -0,0 +1,9 @@
+import numpy as np 
+#Applicable only for Rod elements
+def ASSIGNMENT_MATRIX(element_number,number_of_elements):
+    assignment_matrix=np.zeros([2,number_of_elements+1])
+    assignment_matrix[0,element_number-1]=1
+    assignment_matrix[1,element_number]=1
+    return assignment_matrix
+#assignment_matrix=ASSIGNMENT_MATRIX(10,10)
+#print(assignment_matrix)
