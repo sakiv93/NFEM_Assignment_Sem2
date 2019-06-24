01bd244613878a7366d399d43e3807cb5125bd4f
sakiv93 <vikas.sakiv93@gmail.com>
2019-06-24 02:09:26 +0200

    Material properties as in assignment description

---
+++ material_parameters.py
@@ -0,0 +1,5 @@
+#Material properties
+youngs_modulus=                 70                   #MPa
+poissons_ratio=                 0.25
+yield_stress=                   70                   #MPa
+external_volumetric_strain=     0.1
