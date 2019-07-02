c3d05c3f6c67fee4257c3a59359b753948531df2
nikhilbauskar <49998567+nikhilbauskar@users.noreply.github.com>
2019-07-02 10:42:49 +0200

    Material Element routines with main program

--- Material_Routine.py
+++ Material_Routine.py
@@ -20,6 +20,12 @@ def materialRoutine(epsilon,epsilon_pl, T_m):
     print(sigma_equivalent_trial,sigma_y)
 
     #delta_lamda = sigma_equivalent_trial/(3*mu + sigma_y)
+    if sigma_equivalent_trial > sigma_y:
+        delta_lamda = sigma_equivalent_trial/(3*mu + sigma_y)
+    elif sigma_equivalent_trial <= sigma_y:
+        delta_lamda = 0.
+
+    #delta_lamda = sigma_equivalent_trial/(3*mu + sigma_y)
     if sigma_equivalent_trial==0:
         sigma_updated=np.zeros([3,1])
         Ct_1 = (3*lamda+2*mu) / 3
@@ -51,6 +57,26 @@ def materialRoutine(epsilon,epsilon_pl, T_m):
 
     epsilon_pl = epsilon - np.transpose(np.matmul(np.transpose(sigma_updated), np.linalg.inv(C_el)))
 
-    return Ct_out, sigma_updated, epsilon_pl
+    return Ct_out, sigma_updated, epsilon_pl        
+    
+#     with np.errstate(divide='ignore'):
+#         sigma_updated = (1/3 * np.ones([3,1]) * np.sum(sigma_trial)) + sigma_dev_trial*(1-(3*mu*delta_lamda/sigma_equivalent_trial))
 
-#print(materialRoutine(np.array([[1],[0],[2]]),np.array([[0],[0],[0]]), 0.1))
+#     Ct_1 = (3*lamda+2*mu) / 3
+#     Ct_2 = (4*mu/3) * (1-(3*mu*delta_lamda/sigma_equivalent_trial))
+#     Ct_2_nd = (-1*mu/3) * (1-(3*mu*delta_lamda/sigma_equivalent_trial))
+#     Ct_3 = ((3*mu) / (sigma_equivalent_trial**2))* np.matmul(sigma_dev_trial,np.transpose(sigma_dev_trial))
+
+
+
+#     Ct = np.array([[Ct_1 + Ct_2, Ct_2_nd, Ct_2_nd],
+#                     [Ct_2_nd, Ct_1 + Ct_2, Ct_2_nd],
+#                     [Ct_2_nd, Ct_2_nd, Ct_1 + Ct_2]
+#                     ]) 
+#     Ct_out = Ct + Ct_3 
+
+#     epsilon_pl = epsilon - np.transpose(np.matmul(np.transpose(sigma_updated), np.linalg.inv(C_el)))
+
+#     return Ct_out, sigma_updated, epsilon_pl
+
+# #print(materialRoutine(np.array([[1],[0],[2]]),np.array([[0],[0],[0]]), 0.1))
--- main_program.py
+++ main_program.py
@@ -11,30 +11,25 @@ from assignment_matrix import ASSIGNMENT_MATRIX
 from Element_Routine import elementRoutine
 
 #solving parameters
-number_of_steps=1
-initial_tau=0
-final_tau=1
-delta_t=(final_tau-initial_tau)/number_of_steps
-tau=0.0
-E_pls=np.zeros([3,1])
-nElem=number_of_elements
-
+number_of_steps = 100
+initial_tau = 0
+final_tau = 1
+delta_t = (final_tau-initial_tau)/number_of_steps
+tau= 0.0
+E_pls = np.zeros([3,1])
+nElem = number_of_elements
 
 #initialization of time like parameter and displacement vector
 time=np.array([0])
 U_g_0=np.zeros((nElem+1,1))
-U_gg=U_g_0 
 
 #loop for iterating load from 0% to 100% with user defined start,stop,minimum,maximum
 for i in range(number_of_steps+1):
-    tau=delta_t*(i+1)          #see if tau values are generating properly
+    tau=delta_t*i          #see if tau values are generating properly
     #U_g=U_g_0[-1]
-    #U_g=U_g_0 
-    U_g=U_gg[-1]
-    U_g[0]=1/3*E_v*tau*10 
+    U_g=U_g_0  
     #append U_g_0 at the end of iteration
-    dU_g=np.zeros((nElem+1,1))
-    
+    ##### dU_g=np.zeros((nElem+1,1))
 
     #DO Newton_Raphson_method
     k=1
@@ -46,13 +41,14 @@ for i in range(number_of_steps+1):
             A=ASSIGNMENT_MATRIX(j+1,nElem)
             U_e=np.matmul(A,U_g)
             # calling Element routine 
-            K_e,F_e_int,F_e_ext,E_pl=elementRoutine(U_e,tau,rnodes[j:j+2],E_pls[-1])
+            K_e,F_e_int,F_e_ext,E_pl=elementRoutine(U_e,tau,rnodes[j:j+2],E_pls[-1])  #Eplsilon_plastic to be saved globally
+            print(tau)
             Kt_g=Kt_g+np.matmul(np.transpose(A),np.matmul(K_e,A))
             G_global=G_global+np.matmul(np.transpose(A),(F_e_int-F_e_ext))
             F_g_int=F_g_int+np.matmul(np.transpose(A),(F_e_int))
         #Implementation of essential boundary conditions in K and G
         #G_global[0,0]=-10
-        #U_g[0,0]=1/3*E_v*tau*10
+        U_g[0,0]=1/3*E_v*tau*10    # have to chane U_g assignment
         #print(U_g)
         #Reduced system of equations
         K_rg=Kt_g
@@ -64,14 +60,17 @@ for i in range(number_of_steps+1):
         dU_g=np.matmul(np.linalg.inv(K_rg),-reduced_G_global)
         U_g[1:]=U_g[1:]+dU_g
         k=k+1
-        #print(k) 
+        
         
         #Implementation of essential boundary conditions in U_g
         if (np.linalg.norm(G_global)<=0.005*np.linalg.norm(F_g_int) or np.linalg.norm(dU_g)<=0.005*np.linalg.norm(U_g)) or k>=5:
             break
-    U_gg=np.append([U_g],[U_g],axis=0)
-    print(U_g)
-    E_pls=np.append(E_pls,E_pl,axis=1)
-    print(U_gg)
+    print(k) 
+    U_g_0 = U_g
+    E_pls = E_pl
+print(U_g_0)
+    #U_g_0=np.append(U_g_0,U_g,axis=0)              # Appending to be take care of
+    #E_pls=np.append(E_pls,E_pl,axis=1)             # Appending to be take care of
+    #print(U_g)
 
 
