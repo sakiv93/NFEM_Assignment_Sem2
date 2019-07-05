b007072268d2f5322ba1fa5493d94accd4542296
sakiv93 <vikas.sakiv93@gmail.com>
2019-07-05 16:28:03 +0200

    with changed C condition for elastic

--- Material_Routine.py
+++ Material_Routine.py
@@ -17,48 +17,100 @@ def materialRoutine(epsilon,epsilon_pl, T_m):
     sigma_dev_trial = sigma_trial - 1/3 * (np.sum(sigma_trial))
     sigma_equivalent_trial = (1.5*np.sum(sigma_dev_trial*sigma_dev_trial))**0.5
     
-    print(sigma_equivalent_trial,sigma_y)
+    #print(sigma_equivalent_trial,sigma_y)
 
     #delta_lamda = sigma_equivalent_trial/(3*mu + sigma_y)
     if sigma_equivalent_trial > sigma_y:
         delta_lamda = sigma_equivalent_trial/(3*mu + sigma_y)
-    elif sigma_equivalent_trial <= sigma_y:
-        delta_lamda = 0.
-
-    #delta_lamda = sigma_equivalent_trial/(3*mu + sigma_y)
-    if sigma_equivalent_trial==0:
-        sigma_updated=np.zeros([3,1])
-        Ct_1 = (3*lamda+2*mu) / 3
-        Ct_2=0
-        Ct_2_nd=0
-        Ct_3=0
-    else:
-        if sigma_equivalent_trial > sigma_y:
-            delta_lamda = sigma_equivalent_trial/(3*mu + sigma_y)
-        elif sigma_equivalent_trial <= sigma_y:
-            delta_lamda = 0.
-
-    
-
         sigma_updated = (1/3 * np.ones([3,1]) * np.sum(sigma_trial)) + sigma_dev_trial*(1-(3*mu*delta_lamda/sigma_equivalent_trial))
-
         Ct_1 = (3*lamda+2*mu) / 3
         Ct_2 = (4*mu/3) * (1-(3*mu*delta_lamda/sigma_equivalent_trial))
         Ct_2_nd = (-2*mu/3) * (1-(3*mu*delta_lamda/sigma_equivalent_trial))
-        Ct_3 = ((3*mu) / (sigma_equivalent_trial**2))* np.matmul(sigma_dev_trial,np.transpose(sigma_dev_trial))
-
-
-
-    Ct = np.array([[Ct_1 + Ct_2, Ct_1+Ct_2_nd, Ct_1+Ct_2_nd],
+        Ct_3 = ((3*mu) / (sigma_equivalent_trial**2))* np.matmul(sigma_dev_trial,np.transpose(sigma_dev_trial))        
+        
+        Ct = np.array([[Ct_1 + Ct_2, Ct_1+Ct_2_nd, Ct_1+Ct_2_nd],
                     [Ct_1+Ct_2_nd, Ct_1 + Ct_2, Ct_1+Ct_2_nd],
                     [Ct_1+Ct_2_nd, Ct_1+Ct_2_nd, Ct_1 + Ct_2]
                     ]) 
-    Ct_out = C_el #Ct + Ct_3 
 
-    epsilon_pl = epsilon - np.transpose(np.matmul(np.transpose(sigma_updated), np.linalg.inv(C_el)))
-
-    return Ct_out, sigma_updated, epsilon_pl        
+    elif sigma_equivalent_trial <= sigma_y:
+        delta_lamda = 0.
+        sigma_updated=sigma_trial
+        Ct=C_el
     
+    
+    epsilon_pl = epsilon - np.matmul(np.linalg.inv(Ct),(sigma_updated))
+
+    return Ct, sigma_updated, epsilon_pl        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # #delta_lamda = sigma_equivalent_trial/(3*mu + sigma_y)
+    # if sigma_equivalent_trial==0:
+    #     sigma_updated=np.zeros([3,1])
+    #     Ct_1 = (3*lamda+2*mu) / 3
+    #     Ct_2=0
+    #     Ct_2_nd=0
+    #     Ct_3=0
+    # else:
+    #     if sigma_equivalent_trial > sigma_y:
+    #         delta_lamda = sigma_equivalent_trial/(3*mu + sigma_y)
+    #     elif sigma_equivalent_trial <= sigma_y:
+    #         delta_lamda = 0.
+
+    
+
+    #     sigma_updated = (1/3 * np.ones([3,1]) * np.sum(sigma_trial)) + sigma_dev_trial*(1-(3*mu*delta_lamda/sigma_equivalent_trial))
+
+    #     Ct_1 = (3*lamda+2*mu) / 3
+    #     Ct_2 = (4*mu/3) * (1-(3*mu*delta_lamda/sigma_equivalent_trial))
+    #     Ct_2_nd = (-2*mu/3) * (1-(3*mu*delta_lamda/sigma_equivalent_trial))
+    #     Ct_3 = ((3*mu) / (sigma_equivalent_trial**2))* np.matmul(sigma_dev_trial,np.transpose(sigma_dev_trial))
+
+
+
+    # Ct = np.array([[Ct_1 + Ct_2, Ct_1+Ct_2_nd, Ct_1+Ct_2_nd],
+    #                 [Ct_1+Ct_2_nd, Ct_1 + Ct_2, Ct_1+Ct_2_nd],
+    #                 [Ct_1+Ct_2_nd, Ct_1+Ct_2_nd, Ct_1 + Ct_2]
+    #                 ]) 
+    # Ct_out = C_el #Ct + Ct_3 
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #     with np.errstate(divide='ignore'):
 #         sigma_updated = (1/3 * np.ones([3,1]) * np.sum(sigma_trial)) + sigma_dev_trial*(1-(3*mu*delta_lamda/sigma_equivalent_trial))
 
